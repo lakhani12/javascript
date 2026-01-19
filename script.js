38d7477cbd6346b1db1a3ccfce4a402809541a2d
lakhani12 <hitlakhani3@gmail.com>
2026-01-19 17:43:26 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+//variable  -- temp data store (ex.flipkart,amazon taht save your date buying purpuse)
+
+//var,let and const -- line by line comparison
+// var(ES5)
+var a = "temp";
+var _123 = "Hello";
+var $123 = "Hello";
+b = 24; // never use
+//let
+let temp = "abc";
+//const
+const temp_const = "Hello World!!";
